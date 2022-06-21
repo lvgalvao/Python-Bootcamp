8163ec1d68b4bb75d852dcb10f9bbb7ce0795c9d
Luciano Filho <lvgalvaofilho@gmail.com>
2022-06-20 23:35:41 -0300

    exercicio 03 do dia 03 finalizado

--- day-03.py
+++ day-03.py
@@ -1,14 +1,28 @@
 # # inicio do dia 03
-# height = int(input("Qual sua altura?"))
-# if height != 190:
-#     print("Pode ir")
+# # height = int(input("Qual sua altura?"))
+# # if height != 190:
+# #     print("Pode ir")
+# # else:
+# #     print("Não pode ir")
+
+# # criar um programa para avaliar se é ímpar ou par
+# var = int(input("insira um numero"))
+
+# if var % 2 == 1:
+#     print("impar")
 # else:
-#     print("Não pode ir")
+#     print("par")
 
-# criar um programa para avaliar se é ímpar ou par
-var = int(input("insira um numero"))
+peso = int(input("qual o seu peso?"))
 
-if var % 2 == 1:
-    print("impar")
+if peso > 50:
+    print("voce é pesado")
+    idade = int(input("qual sua idade?"))
+    if idade > 18:
+        print("voce pode dirigir")
+    else:
+        print("voce é magro e novo")
+elif peso > 30:
+    print("voce é médio")
 else:
-    print("par")
+    print("voce é magro")
