200aa76a9f6857ce1972719f9c7b4d27801e56d4
Luciano Filho <lvgalvaofilho@gmail.com>
2022-06-20 23:26:35 -0300

    exercicio 02 do dia 03 finalizado

---
+++ day-03.py
@@ -0,0 +1,14 @@
+# # inicio do dia 03
+# height = int(input("Qual sua altura?"))
+# if height != 190:
+#     print("Pode ir")
+# else:
+#     print("Não pode ir")
+
+# criar um programa para avaliar se é ímpar ou par
+var = int(input("insira um numero"))
+
+if var % 2 == 1:
+    print("impar")
+else:
+    print("par")
