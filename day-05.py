d9b93384fae15c7a03b3037ea8cd1acbd21bee8e
Luciano Filho <lvgalvaofilho@gmail.com>
2022-06-23 23:38:22 -0300

    dia 05

---
+++ day-05.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+soma_student = 0
+for n in student_heights[n]:
+    soma_student += student_heights[n]
+
+#Write your code below this row 👇
