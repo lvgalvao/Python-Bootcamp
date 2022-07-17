d91aaa386845dbd956b5995117a7d9b78663fea8
Luciano Filho <lvgalvaofilho@gmail.com>
2022-07-17 09:11:42 -0300

    inicio day08

--- day-08.py
+++ day-08.py
@@ -1,11 +1,16 @@
-def greet():
-    print("hello")
-    print("how are you?")
-    print("what is your name?")
+# def greet():
+# print("hello")
+# print("how are you?")
+# print("what is your name?")
 
-def greet_with_name(name):
-    print(f"hello {name}")
-    print(f"how are you {name}?")
-    print(f"what is your {name}")
+# def greet_with_name(name):
+# print(f"hello {name}")
+# print(f"how are you {name}?")
+# print(f"what is your {name}")
 
-greet_with_name("Jorge")
+# greet_with_name("Jorge")
+
+def greet_with_two_parameters(name, location)
+    print(f"hello {name} are you from {location}")
+
+greet_with_two_parameters("Jorge", "Brasil")
