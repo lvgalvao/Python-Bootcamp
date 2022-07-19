ba0c636fc6af09fc1f75bafe8737cf107f8485cd
Luciano Filho <lvgalvaofilho@gmail.com>
2022-07-19 06:30:42 -0300

    start next challange

--- day-09.py
+++ day-09.py
@@ -1,34 +1,42 @@
-student_scores = {
-  "Harry": 81,
-  "Ron": 78,
-  "Hermione": 99, 
-  "Draco": 74,
-  "Neville": 62,
+# student_scores = {
+#   "Harry": 81,
+#   "Ron": 78,
+#   "Hermione": 99, 
+#   "Draco": 74,
+#   "Neville": 62,
+# }
+# # 🚨 Don't change the code above 👆
+
+# #TODO-1: Create an empty dictionary called student_grades.
+
+# student_grades = {
+# }
+
+# #TODO-2: Write your code below to add the grades to student_grades.👇
+
+# # Score 91-100 grade = "Outstanding"
+# # Score 81-90 grade = "Exceeds Expectations"
+# # Score 71-80 grade = "Acceptable"
+# # Score 0-70 grade = "Normal"
+
+# for student in student_scores:
+#     score = student_scores[student]
+#     if score >= 91:
+#         student_grades[student] = "Outstanding"
+#     elif score >= 81:
+#         student_grades[student] = "Exceeds Expectations"
+#     elif score >= 71:
+#         student_grades[student] = "Acceptable"
+#     elif score>= 0:
+#         student_grades[student] = "Normal"
+
+# # 🚨 Don't change the code below 👇
+# print(student_grades)
+
+starting_dictionary = {
+    "a": 9,
+    "b": 8,
 }
-# 🚨 Don't change the code above 👆
 
-#TODO-1: Create an empty dictionary called student_grades.
-
-student_grades = {
-}
-
-#TODO-2: Write your code below to add the grades to student_grades.👇
-
-# Score 91-100 grade = "Outstanding"
-# Score 81-90 grade = "Exceeds Expectations"
-# Score 71-80 grade = "Acceptable"
-# Score 0-70 grade = "Normal"
-
-for student in student_scores:
-    score = student_scores[student]
-    if score >= 91:
-        student_grades[student] = "Outstanding"
-    elif score >= 81:
-        student_grades[student] = "Exceeds Expectations"
-    elif score >= 71:
-        student_grades[student] = "Acceptable"
-    elif score>= 0:
-        student_grades[student] = "Normal"
-
-# 🚨 Don't change the code below 👇
-print(student_grades)
+starting_dictionary["c"] = 7
+print(starting_dictionary)
