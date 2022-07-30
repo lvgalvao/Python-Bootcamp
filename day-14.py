d199386b5b0435f93e35bbea92b0ac58231c6cf4
Luciano Filho <lvgalvaofilho@gmail.com>
2022-07-30 05:10:02 -0300

    revisao tuplas

--- day-14.py
+++ day-14.py
@@ -2,13 +2,13 @@
 
 from artday14 import logo, vs
 from datagameday14 import data
+import random
 
-print(logo)
-print(vs)
-print(data[1].items())
+# print(logo)
+# print(vs)
 
-for e in data:
-    for k,v in e.items():
-        print(f'{k} e {v}')
+def data_random () {
+    return data[random.randint(1,51)]
+    select2 = data[random.randint(1,51)]
+}
 
-    
