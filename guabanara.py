4e6780a1b55f4d1fc4491fe1e893b55541e89456
Luciano Filho <lvgalvaofilho@gmail.com>
2022-07-30 06:23:20 -0300

    atividade 74 finalizada

--- guabanara.py
+++ guabanara.py
@@ -1,31 +1,41 @@
-#72
+# #72
 
-cont = ('zero','um', 'dois', 'tres', 'quatro',
-        'cinco', 'seis', 'sete', 'oito', 'nove', 'dez')
+# cont = ('zero','um', 'dois', 'tres', 'quatro',
+#         'cinco', 'seis', 'sete', 'oito', 'nove', 'dez')
 
-# input a number between 0 and 5
+# # input a number between 0 and 5
 
-# while True:
-#     num_seleted = int(input(print("input a number between 0 and 5")))
-#     if 0 <= num_seleted <= 5:
-#         print("you selected a validad number")
-#         break
-#     else: 
-#         print("you selected a bad number, choise again")
+# # while True:
+# #     num_seleted = int(input(print("input a number between 0 and 5")))
+# #     if 0 <= num_seleted <= 5:
+# #         print("you selected a validad number")
+# #         break
+# #     else: 
+# #         print("you selected a bad number, choise again")
 
 
-# show the top >= 5
-print(cont[:5])
+# # show the top >= 5
+# print(cont[:5])
 
-# show buttom <= 4
-print(cont[-4:])
+# # show buttom <= 4
+# print(cont[-4:])
 
-# show the tuppla sort by asc
-# sort_tuple_list = tuple(sorted(cont, reverse=True))
-# sort_tuple_list = tuple(sorted(cont))
-# print(sort_tuple_list)
-print(sorted(cont))
+# # show the tuppla sort by asc
+# # sort_tuple_list = tuple(sorted(cont, reverse=True))
+# # sort_tuple_list = tuple(sorted(cont))
+# # print(sort_tuple_list)
+# print(sorted(cont))
 
-# how is the index from 'cinco'
-print(cont.index('cinco'))
+# # how is the index from 'cinco'
+# print(cont.index('cinco'))
 
+# 73 init
+
+from random import randint
+numeros = (randint(1, 10),randint(1, 10),randint(1, 10),randint(1, 10),randint(1, 10))
+
+# for cont in numeros:
+#     print('f{cont}', end='')
+
+print(numeros)
+print(sorted(numeros))
