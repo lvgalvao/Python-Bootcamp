401b2c5797be496cb433746f824fc2edb81ac578
Luciano Filho <lvgalvaofilho@gmail.com>
2022-08-05 06:08:12 -0300

    atualziacoes

--- day-15.py
+++ day-15.py
@@ -30,12 +30,54 @@ resources = {
     "coffee": 100,
 }
 
+currenty_resources = dict()
+currenty_resources = resources
+
 
 # inicio do day 15
-
 #create a coffee machine
+looping = True
+
+while looping:
+# choose = input("What would you like? (espresso/latte/cappuccino): ")
+    choose = input("What would you like? (espresso/latte/cappuccino): ")
+    if choose == "report":
+        print("The resources are:")
+        print(f"You have {currenty_resources['water']}ml of water")
+        print(f"You have {currenty_resources['milk']}ml of milk")
+        print(f"You have {currenty_resources['coffee']}ml of coffee")
+    elif choose == "stop":
+        looping = False
+    elif choose == "espresso":
+        # Compare resourcers if espresso < resourcers than consumin and serv")
+        if (MENU["espresso"]["ingredients"]["water"]) <= currenty_resources["water"]: 
+            if (MENU["espresso"]["ingredients"]["coffee"]) <= currenty_resources["coffee"]:
+                print("Tome o seu espresso")
+                for key in currenty_resources:   
+                    currenty_resources[key] -= MENU["espresso"]["ingredients"][key]
+                    print(currenty_resources) 
+            else:
+                print("Sem recurso disponível")
+        else:
+            print("Sem recurso disponível")    
+
+
+
+
+
+
+
+
+
+
+print("Please insert coins.")
+qte_quarters = int(input("How many quarters? "))
+qte_dimes = int(input("How many dimes? "))
+qte_nickles = int(input("How many nickles? "))
+qte_pennies =int(input("How many pennies? "))
 
 #print report
+
 
 #check resources sufficient
 
@@ -61,3 +103,4 @@ qte_nickles = int(input("How many nickles? "))
 qte_pennies =int(input("How many pennies? "))
 
 exchange(5, qte_quarters, qte_dimes, qte_nickles, qte_pennies)
+
