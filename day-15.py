0ea4a76726cea9302b994b4f718c152d29670300
Luciano Filho <lvgalvaofilho@gmail.com>
2022-08-06 07:04:41 -0300

    avancando dia 15

--- day-15.py
+++ day-15.py
@@ -1,3 +1,5 @@
+
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -25,7 +27,7 @@ MENU = {
 }
 
 resources = {
-    "water": 300,
+    "water": 0,
     "milk": 200,
     "coffee": 100,
 }
@@ -34,73 +36,39 @@ currenty_resources = dict()
 currenty_resources = resources
 
 
+
 # inicio do day 15
 #create a coffee machine
 looping = True
 
+def is_resources_sufficient(order_ingredients):
+    """Returns True when order can be made, False if the ingredients are insufficient"""
+    for item in order_ingredients:
+        if order_ingredients[item] >= currenty_resources[item]:
+            print(f"Sorry there is not {item} enought")
+            return False
+    return True
+
+def process_coins():
+    """Returns the total calculated from coins inserted. """
+    print("Please insert coins.")
+    total = int(input("How many quarters? ")) * 0.25
+    total += int(input("How many dimes? ")) * 0.10
+    total += int(input("How many nickles? ")) * 0.05
+    total +=int(input("How many pennies? ")) * 0.01
+    return total
+
 while looping:
 # choose = input("What would you like? (espresso/latte/cappuccino): ")
-    choose = input("What would you like? (espresso/latte/cappuccino): ")
-    if choose == "report":
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "report":
         print("The resources are:")
         print(f"You have {currenty_resources['water']}ml of water")
         print(f"You have {currenty_resources['milk']}ml of milk")
         print(f"You have {currenty_resources['coffee']}ml of coffee")
-    elif choose == "stop":
+    elif choice == "stop":
         looping = False
-    elif choose == "espresso":
-        # Compare resourcers if espresso < resourcers than consumin and serv")
-        if (MENU["espresso"]["ingredients"]["water"]) <= currenty_resources["water"]: 
-            if (MENU["espresso"]["ingredients"]["coffee"]) <= currenty_resources["coffee"]:
-                print("Tome o seu espresso")
-                for key in currenty_resources:   
-                    currenty_resources[key] -= MENU["espresso"]["ingredients"][key]
-                    print(currenty_resources) 
-            else:
-                print("Sem recurso disponível")
-        else:
-            print("Sem recurso disponível")    
-
-
-
-
-
-
-
-
-
-
-print("Please insert coins.")
-qte_quarters = int(input("How many quarters? "))
-qte_dimes = int(input("How many dimes? "))
-qte_nickles = int(input("How many nickles? "))
-qte_pennies =int(input("How many pennies? "))
-
-#print report
-
-
-#check resources sufficient
-
-# def report_quantity():
-
-
-def exchange (cost, quarters, dimes, nickles, penies):
-    total_money_input = quarters * 0.25 + dimes * 0.10 + nickles * 0.05 + penies * 0.01
-    if total_money_input > cost:
-        troco = total_money_input - cost
-        print(f"você terá {troco}")
     else:
-        print("dinheiro insuficiente")
-
-# choose = input("What would you like? (espresso/latte/cappuccino): ")
-
-# process coins
-
-print("Please insert coins.")
-qte_quarters = int(input("How many quarters? "))
-qte_dimes = int(input("How many dimes? "))
-qte_nickles = int(input("How many nickles? "))
-qte_pennies =int(input("How many pennies? "))
-
-exchange(5, qte_quarters, qte_dimes, qte_nickles, qte_pennies)
-
+        drink = MENU[choice]
+        if is_resources_sufficient(drink["ingredients"]):
+            process_coins()
