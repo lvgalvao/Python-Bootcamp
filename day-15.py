09edc694175d8129c1534566273a92125879b53a
Luciano Filho <lvgalvaofilho@gmail.com>
2022-08-06 07:35:34 -0300

    dia 15 finish

--- day-15.py
+++ day-15.py
@@ -1,5 +1,3 @@
-
-
 MENU = {
     "espresso": {
         "ingredients": {
@@ -27,17 +25,18 @@ MENU = {
 }
 
 resources = {
-    "water": 0,
+    "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
+profit = 0
 currenty_resources = dict()
 currenty_resources = resources
 
 
 
-# inicio do day 15
+# inicio do day53 15
 #create a coffee machine
 looping = True
 
@@ -58,6 +57,36 @@ def process_coins():
     total +=int(input("How many pennies? ")) * 0.01
     return total
 
+def is_transaction_successful(money_received, drink_cost):
+    """Return True when the payment is accepted, or False if money is insufficiente."""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is {change}")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources."""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"here is your {drink_name} ☕ ")
+
+def make_refil(resources, current_money):
+    """Add ingredients to the machine for 1$"""
+    global profit
+    cost_refil = 1
+    if profit >= cost_refil:
+        for item in resources:
+            resources[item] += 300
+        profit -=cost_refil
+        print(f"now your machine has coffee")
+    else:
+        print("You need money to the refil")
+
 while looping:
 # choose = input("What would you like? (espresso/latte/cappuccino): ")
     choice = input("What would you like? (espresso/latte/cappuccino): ")
@@ -66,9 +95,14 @@ while looping:
         print(f"You have {currenty_resources['water']}ml of water")
         print(f"You have {currenty_resources['milk']}ml of milk")
         print(f"You have {currenty_resources['coffee']}ml of coffee")
+        print(f"You have {profit} $ profit")
     elif choice == "stop":
         looping = False
+    elif choice == "refil":
+        make_refil(currenty_resources, profit)
     else:
         drink = MENU[choice]
         if is_resources_sufficient(drink["ingredients"]):
-            process_coins()
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
