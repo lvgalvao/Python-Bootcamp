c43c7d0463c73301499ad5bb7d5580bd5c0e7bcf
Luciano Filho <lvgalvaofilho@gmail.com>
2022-08-07 20:30:38 -0300

    aula16med

---
+++ day-16.py
@@ -0,0 +1,15 @@
+#inicio do dia 16
+
+import another_module
+
+print(another_module.anothervariable)
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
