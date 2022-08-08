bbd3cebbbe4bd2a68d98c9a9598d7bd2d8ebd394
Luciano Filho <lvgalvaofilho@gmail.com>
2022-08-08 07:34:44 -0300

    pretty

--- day-16.py
+++ day-16.py
@@ -1,15 +1,28 @@
 #inicio do dia 16
 
-import another_module
+# import another_module
 
-print(another_module.anothervariable)
+# print(another_module.anothervariable)
 
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.pencolor("blue")
+# timmy.fillcolor("coral")
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+print(table)
+
+table.field_names = ["Pokemon Name", "Type"]
+table.add_row(["Pikachu", "Electric"])
+table.add_row(["Squirtle", "Water"])
+table.add_row(["Charmander", "Fire"])
