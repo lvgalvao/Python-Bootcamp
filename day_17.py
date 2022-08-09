b80cd866683a58efa3ee92c3ca663d000de070e1
Luciano Filho <lvgalvaofilho@gmail.com>
2022-08-09 06:08:06 -0300

    156

--- day_17.py
+++ day_17.py
@@ -5,9 +5,25 @@ class User:
         self.id = user_id
         self.name = username
         self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
 
 user_1 = User("001", "Luciano")
 print(user_1.followers)
+
+user_2 = User("002", "Carla")
+print(user_2.followers)
+
+user_1.follow(user_2)
+
+print(user_1.following)
+print(user_1.followers)
+print(user_2.following)
+print(user_2.followers)
 
 # def function():
 #     pass
