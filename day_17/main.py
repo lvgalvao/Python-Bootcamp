8fadf0f11e1525bc41edc0b7e8ab09e952b7a66d
Luciano Filho <lvgalvaofilho@gmail.com>
2022-08-09 12:17:43 -0300

    day17

--- day_17/main.py
+++ day_17/main.py
@@ -1,6 +1,19 @@
 #Quiz project
 
 from data import question_data
+from question_model import Questions
 
-for key in question_data[1].keys():
-    print(key)
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Questions(question_text, question_answer)
+    question_bank.append(new_question)
+
+#TODO asking the questions
+
+que = question_bank.sort()
+print(que["text"])
+
+#TODO checking if the answer was correct
+#TODO checking if we're the end of the quiz
--- day_17/question_model.py
+++ day_17/question_model.py
@@ -2,6 +2,3 @@ class Questions:
     def __init__(self, q_text, q_question) -> None:
         self.text = q_text
         self.question = q_question
-
-new_q = Questions("th","dhaais")
-print(f" print it {new_q.text} and {new_q.question}")
