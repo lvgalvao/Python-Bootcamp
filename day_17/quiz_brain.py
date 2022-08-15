3b8d229d7c2a7f01f776ceac5ef95387b4874626
Luciano Filho <lvgalvaofilho@gmail.com>
2022-08-15 05:15:44 -0300

    ini 177

--- day_17/quiz_brain.py
+++ day_17/quiz_brain.py
@@ -1,4 +1,10 @@
 class QuizBrain:
+
     def __init__(self, q_list) -> None:
         self.question_list = q_list
         self.question_number = 0
+
+    def next_question(self):
+        current_question = self.question_list(self.question_number)
+        self.question_number += 1
+        input(f"Q.{self.question_number""})
