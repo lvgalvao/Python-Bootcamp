b974c9efc97d07a0684d9b2bd257a2fae20b39a4
Luciano Filho <lvgalvaofilho@gmail.com>
2022-08-19 22:42:53 -0300

    novo

---
+++ day18.py
@@ -0,0 +1,19 @@
+# #day18
+
+# from turtle import Turtle, Screen
+
+# timmy_the_turtle = Turtle()
+
+# screen = Screen()
+# screen.exitonclick()
+
+
+# Python program to demonstrate the use of floor() method
+  
+# This will import math module
+import math   
+  
+# prints the ceil using floor() method
+print(f'math.floor(-23.11) : , {math.floor(-23.11)}')
+# print "math.floor(300.16) : ", math.floor(300.16)
+# print "math.floor(300.72) : ", math.floor(300.72)
