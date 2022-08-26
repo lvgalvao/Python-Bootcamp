0a0241cd88557efdcfff07e9a3a4b41e8b01faaf
Luciano Filho <lvgalvaofilho@gmail.com>
2022-08-26 05:43:25 -0300

    voltando ao eixo

--- day18.py
+++ day18.py
@@ -1,17 +1,11 @@
-import json
-import requests
-
-url = requests.get("https://intellifood-au-data.s3.us-west-2.amazonaws.com/consolidated-data/Grocery/3/2022/08/17/12/008698e4-e62b-4d08-b289-ea8350aa8287.json")
-print(url.status_code)
-print(url.headers["content-type"])
-print(url.encoding)
-print(type(url))
-
-dic = url.json()
-
-print(dic)
-# x =  '{ "name":"John", "age":30, "city":"New York"}'
-# print(type(x))
-# y = json.loads(x)
-# print(type(y))
-# print(y["age"])
+from turtle import *
+shape('turtle')
+color('red', 'yellow')
+begin_fill()
+home()
+speed(1)
+goto(100, 100)
+circle(50)
+stamp()
+circle(120, 180)  # draw a semicircle
+fd(500)
