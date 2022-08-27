aed02001680b8b3b7554e61398e18789a002845b
Luciano Filho <lvgalvaofilho@gmail.com>
2022-08-27 05:10:35 -0300

    ex03

--- day18.py
+++ day18.py
@@ -1,1 +1,13 @@
-print("hello")
+from turtle import *
+import turtle as tur
+
+tur.shape("turtle")
+tur.color("red")
+
+for i in range(0,200):
+    tur.fd(10)
+    tur.penup()
+    tur.fd(10)
+    tur.pendown()
+
+tur.exitonclick()
