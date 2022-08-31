5c1ff0996c72dc211256260a4b5bf8183b9559e5
Luciano Filho <lvgalvaofilho@gmail.com>
2022-08-30 23:00:26 -0300

    atualizacao

--- day19.py
+++ day19.py
@@ -3,7 +3,9 @@ from itertools import permutations
 
 cel = re.compile(r'[7-9][0-9]{7}')
 
-for tel in permutations(range(1,10),8):
-    _tel = ''.join(str(x) for x in tel)
-    if cel.search(_tel):
-        print(_tel)
+# for tel in permutations(range(1,10),8):
+#     _tel = ''.join(str(x) for x in tel)
+#     if cel.search(_tel):
+#         print(_tel)
+
+print(permutations(range(1,10),8))
