86ccc95edf405b504f338fd6e13c3b7594c4eeac
Luciano Filho <lvgalvaofilho@gmail.com>
2022-09-04 14:00:53 -0300

    task 03 finish

---
+++ project 21 snack game/main.py
@@ -0,0 +1,44 @@
+# task 04 - create snake food
+# task 05 - detect collision with food
+# task 06 - create a scoreboard
+# task 07 - detect collision with wall
+# task 08 - detect collision with tail
+
+from turtle import Turtle, Screen
+from snack import Snake
+import time
+
+# task 01 - setup the screen
+
+screen = Screen()
+screen.screensize(600,600)
+screen.bgcolor("black")
+screen.title(titlestring="Welcome to the Snack Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
