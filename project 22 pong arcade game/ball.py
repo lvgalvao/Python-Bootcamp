bffcd41800ce081685bf7a6c9e8a7813f1b057b0
Luciano Filho <lvgalvaofilho@gmail.com>
2022-09-07 12:28:06 -0300

    add ball module

--- project 22 pong arcade game/ball.py
+++ project 22 pong arcade game/ball.py
@@ -1,1 +1,18 @@
 # task 04 - create the ball and make it move
+
+
+from turtle import Turtle
+
+
+class Ball(Turtle):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+
+    def move(self):
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
+        self.goto(new_x, new_y)
--- project 22 pong arcade game/main.py
+++ project 22 pong arcade game/main.py
@@ -1,27 +1,31 @@
-# task 01 - create the screen
-# task 03 - create another paddle
-# task 05 - detect collision with wall and bounce
-# task 06 - detect collision with paddle
-# task 07 - detect when paddle misses
-
-from turtle import Turtle, Screen
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(800,600)
-screen.title("Pong")
+screen.setup(width=800, height=600)
+screen.title(titlestring="Welcome to Pong")
+screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle_r = Paddle((350,0))
+paddle_l = Paddle((-350,0))
+ball = Ball()
 
 
+screen.listen()
+screen.onkey(paddle_r.go_up, "Up")
+screen.onkey(paddle_r.go_down, "Down")
+screen.onkey(paddle_l.go_up, "w")
+screen.onkey(paddle_l.go_down, "s")
 
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    ball.move()
+
+    
 
 screen.exitonclick()
-
-
-
-
-
