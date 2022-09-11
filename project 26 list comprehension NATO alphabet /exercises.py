fe15e0dbc9dcaf51cd350de89401b48533dc4634
Luciano Filho <lvgalvaofilho@gmail.com>
2022-09-11 06:26:29 -0300

    finish task 01

---
+++ project 26 list comprehension NATO alphabet /exercises.py
@@ -0,0 +1,60 @@
+#list comprehension NATO alphabet
+NUMBERS = [1 ,2 ,3]
+NAMES = ["Alex", "Beth", "Caroline", "Dave", "Elanor", "Freddie"]
+new_numbers_2 = []
+
+#tradicional way to create a new list
+for n in NUMBERS:
+    new_item = n + 1
+    new_numbers_2.append(new_item)
+# print(new_numbers_2)
+
+#pytonic code to create a new list
+new_numbers = [n + 1 for n in NUMBERS]
+# print(new_numbers)
+
+#create a new list from a range, where the list items are double the values in the range
+new_list_double = [n * 2 for n in range(1,4)]
+# print(new_list_double)
+
+#conditional list comprehension
+# new_list = [new item for item in list if item]
+new_list_names = [n for n in NAMES if len(n) <= 4]
+# print(new_list_names)
+
+#conditional list comprehension turn names UPPER
+new_list_names_2 = [n.upper() for n in NAMES if len(n) > 4]
+# print(new_list_names_2)
+
+#squaring numbers using conditional list
+NUMBERS = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+squared_numbers = [n**2 for n in NUMBERS]
+# print(squared_numbers)
+
+#filtering even numbers
+odd_numbers = [n for n in NUMBERS if n%2 == 0]
+# print(odd_numbers)
+
+# Take a look inside file1.txt and file2.txt. They each contain a bunch of numbers, each number on a new line.
+# You are going to create a list called result which contains the numbers that are common in both file
+
+# task 1 , create new list using the numbers in these files
+# task 2 , create a new list when the numbers are commun in both list
+
+with open("/Users/lucianogalvao/Code/100DaysofCode/project 26 list comprehension NATO alphabet /file.txt") as file1:
+    list_file1 = file1.readlines()
+    new_list_file1_striped = []
+    for item in list_file1:
+        new_item_striped = item.strip()
+        new_list_file1_striped.append(new_item_striped)
+
+with open("/Users/lucianogalvao/Code/100DaysofCode/project 26 list comprehension NATO alphabet /file2.txt") as file2:
+    list_file2 = file2.readlines()
+    new_list_file2_striped = []
+    for item in list_file2:
+        new_item_striped_2 = item.strip()
+        new_list_file2_striped.append(new_item_striped_2)
+
+print(new_list_file1_striped)
+print(new_list_file2_striped)
+
