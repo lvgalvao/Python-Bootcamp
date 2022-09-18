b1a6c572c66c76d20b1ff0e6710dc3943895b156
Luciano Filho <lvgalvaofilho@gmail.com>
2022-09-18 07:19:13 -0300

    feat: add UI

--- day-28/main.py
+++ day-28/main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -8,6 +9,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+FILE = "day-28/src/tomato.png"
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -16,3 +18,15 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+root = Tk()
+root.title("Pomodoro")
+root.config(padx=100, pady=50, bg=YELLOW)
+
+tomato_img = PhotoImage(file=FILE)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 112, text= "00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+root.mainloop()
