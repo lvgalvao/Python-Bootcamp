ab8394a1879ced9c38d6bcac6011c4e9e98ae205
Luciano Filho <lvgalvaofilho@gmail.com>
2022-09-27 23:56:32 -0300

    sob

--- day-29/main.py
+++ day-29/main.py
@@ -1,2 +1,10 @@
-#codigo comentado
+from urllib.request import urlopen
+import json
 import pandas as pd
+
+response = urlopen('https://blockchain.info/ticker')
+json_data = response.read().decode('utf-8', 'replace')
+
+d = json.loads(json_data)
+df = pd.json_normalize(d['ARS'])
+
