ebefc91f55ee1c38ca4889b4560f255629a5d540
Luciano Filho <lvgalvaofilho@gmail.com>
2022-10-02 01:33:21 -0300

    project 29 done

--- day-29/main.py
+++ day-29/main.py
@@ -1,25 +1,93 @@
-from curses import window
-from email.mime import image
+import email
 from tkinter import *
 from tkinter import ttk
-root = Tk() ### Creates an instance of the Tk class, 
-            ### which initializes Tk and creates its associated Tcl interpreter. It also creates a toplevel window,
-            ### known as the root window, which serves as the main window of the application.
+from tkinter import messagebox
+import random
+import pyperclip
 
-root.title("Password Manager")
-root.config(padx=20, pady=20)
-
-canvas = Canvas(height=200 , width= 200)
-logo_img = PhotoImage(file="day-29/src/logo.png")
-canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
-root.mainloop() ### Mantein the application 
-just it
+from importlib_metadata import entry_points
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+def generate_password():
+#Password Generator Project
+    password_entry.delete(0, END)
+    password = ""
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    password_letter = [random.choice(letters) for _ in range(nr_letters)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+
+    password_list = password_letter + password_numbers + password_symbols
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save():
+    password = password_entry.get()
+    website = website_entry.get()
+    email = email_entry.get()
+
+    if password == "" and website == "":
+        messagebox.showinfo(message='please insert some values')
+    else:
+        is_ok = messagebox.askokcancel(title='website', message=f'These are the details entered: \nEmail: {email}'
+                                                        f'\nPassword: {password} \nIs it ok to save?')
+
+        if is_ok:
+            with open('day-29/src/readme.txt', 'a') as f:
+                f.write(f'website: {website} email: {email} password {password}\n')
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
-upupupudododi
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="day-29/src/logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+#Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+#Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "lvgalvaofilho@gmail.com")
+password_entry = Entry( width=21)
+password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_password_button = Button(text="Generate password", command=generate_password)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
